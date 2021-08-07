d789eb80fa4dcd057499ae8a8501dd1387012727
Anton Boriskin <antbor23@gmail.com>
2021-08-07 13:49:12 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(4,5));
+        System.out.println(calc.mult(6,3));
+        System.out.println(calc.pow(2,3));
+
+
+    }
+
+}
